44f1459ba1286cec20b0fb7de3dcfbf607315d00
Kyle Hendry <kyle.hendry@digitata.com>
2021-01-31 00:14:06 +0200

    Menu Scripts

---
+++ Assets/MobileMenuController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using Mirror.Discovery;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MobileMenuController : MonoBehaviour
+{
+    public NetworkManager NetworkManager;
+    public GameObject MainMenu;
+    public GameObject Waiting;
+    public Scene gameScene;
+
+    private void Start()
+    {
+        MainMenu.SetActive(true);
+        Waiting.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void StartGame()
+    {
+        NetworkManager.StartClient();
+        MainMenu.SetActive(false);
+        Waiting.SetActive(true);
+    }
+    
+    public void GameConnected(ServerResponse response)
+    {
+        if (NetworkClient.isConnected)
+        {
+            SceneManager.LoadScene(gameScene.name);
+        }
+    }
+}
---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+
+    public NetworkManager NetworkManager;
+    public GameObject MainMenu;
+    public GameObject Waiting;
+    public Scene gameScene;
+
+    private void Start()
+    {
+        MainMenu.SetActive(true);
+        Waiting.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void StartGame()
+    {
+        NetworkManager.StartHost();
+        MainMenu.SetActive(false);
+        Waiting.SetActive(true);
+    }
+
+    public void GameConnected()
+    {
+        SceneManager.LoadScene(gameScene.name);
+    }
+    
+}
